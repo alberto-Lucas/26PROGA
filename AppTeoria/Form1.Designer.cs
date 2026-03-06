7f5da66cc530d92f374261bd00fb217c9f9ec196
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2026-03-05 22:22:22 -0300

    AppTeoria
    Aula dia 05/03/2026

---
+++ AppTeoria/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace AppTeoria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbbOpcoes = new System.Windows.Forms.ComboBox();
+            this.btnIF = new System.Windows.Forms.Button();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.btnExemplo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbbOpcoes
+            // 
+            this.cbbOpcoes.FormattingEnabled = true;
+            this.cbbOpcoes.Items.AddRange(new object[] {
+            "APARTAMENTO",
+            "SUITE",
+            "MASTER",
+            "DIAMANTE",
+            "EXECUTIVA"});
+            this.cbbOpcoes.Location = new System.Drawing.Point(12, 12);
+            this.cbbOpcoes.Name = "cbbOpcoes";
+            this.cbbOpcoes.Size = new System.Drawing.Size(231, 21);
+            this.cbbOpcoes.TabIndex = 0;
+            // 
+            // btnIF
+            // 
+            this.btnIF.Location = new System.Drawing.Point(12, 39);
+            this.btnIF.Name = "btnIF";
+            this.btnIF.Size = new System.Drawing.Size(75, 23);
+            this.btnIF.TabIndex = 1;
+            this.btnIF.Text = "IF";
+            this.btnIF.UseVisualStyleBackColor = true;
+            this.btnIF.Click += new System.EventHandler(this.btnIF_Click);
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Location = new System.Drawing.Point(12, 68);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(75, 23);
+            this.btnSwitch.TabIndex = 2;
+            this.btnSwitch.Text = "SWITCH";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // btnExemplo
+            // 
+            this.btnExemplo.Location = new System.Drawing.Point(93, 68);
+            this.btnExemplo.Name = "btnExemplo";
+            this.btnExemplo.Size = new System.Drawing.Size(75, 23);
+            this.btnExemplo.TabIndex = 3;
+            this.btnExemplo.Text = "Exemplo";
+            this.btnExemplo.UseVisualStyleBackColor = true;
+            this.btnExemplo.Click += new System.EventHandler(this.btnExemplo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 110);
+            this.Controls.Add(this.btnExemplo);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.btnIF);
+            this.Controls.Add(this.cbbOpcoes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbbOpcoes;
+        private System.Windows.Forms.Button btnIF;
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.Button btnExemplo;
+    }
+}
+
---
+++ AppTeoria/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppTeoria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIF_Click(object sender, EventArgs e)
+        {
+            //Aplicar a teoria do IF
+            //Ou seja, iremos realizar uma cascata
+            //de validações
+
+            //O objeto é identificar a opção selecionada
+            //e apresentar ao usuario
+
+            //Criar uma variavel para armazenar o texto
+            string opcao;
+
+            //Utilizar o if quando podemos
+            //ter condições diferentes
+            //Vou validar se a opção selecionada
+            //condiz com a opção defenida
+            if (cbbOpcoes.Text == "APARTAMENTO")
+                opcao = "Opção Apartamento";
+            else if (cbbOpcoes.Text == "SUITE")
+                opcao = "Opção Suite";
+            else if (cbbOpcoes.Text == "MASTER")
+                opcao = "Opção Master";
+            else if (cbbOpcoes.Text == "DIAMANTE")
+                opcao = "Opção Diamente";
+            else if (cbbOpcoes.Text == "EXECUTIVA")
+                opcao = "Opção Executiva";
+            else
+                opcao = "Opção Desconhecida";
+
+            //Apresentar a mensagem ao usuário
+            MessageBox.Show(opcao);
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            //A utilização do switch se da qunado temos
+            //uma variação de um tipo de dados
+
+            //OU seja no IF, temos variação de validações
+            //podendo ter outros dados no meio
+            //ja no switch, tem apenas a variação de um 
+            //tipo de dado
+            //neste caso nosso tipo de dados
+            //são quartos de Motel
+
+            //criar a variavel opcao
+
+            string opcao;
+
+            //aplicar o switch
+            switch(cbbOpcoes.SelectedIndex)
+            {
+                case 0: opcao = "Opção 1";
+                    break; //Obrigado utilizar o break
+                case 1: opcao = "Opção 2";
+                    break;
+                case 2: opcao = "Opção 3";
+                    break; //Obrigado utilizar o break
+                case 3: opcao = "Opção 4";
+                    break;
+                case 4: opcao = "Opção 5";
+                    break;
+                default: opcao = "Opção Diamante";
+                    break;
+            }
+
+            MessageBox.Show(opcao);
+        }
+
+        private void btnExemplo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Opção: " + cbbOpcoes.Text);
+        }
+    }
+}
