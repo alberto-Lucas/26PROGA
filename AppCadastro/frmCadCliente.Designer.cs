e73c732efbe880973332f96fcb1a02f20984c71e
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2026-05-14 22:21:09 -0300

    AppCasdastros - CadCliente

--- AppCadastro/frmCadCliente.Designer.cs
+++ AppCadastro/frmCadCliente.Designer.cs
@@ -28,19 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCadCliente));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtRG = new System.Windows.Forms.TextBox();
+            this.dtpDtNascimento = new System.Windows.Forms.DateTimePicker();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 25);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(289, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(307, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Data Nascimento:";
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(12, 82);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(141, 20);
+            this.txtCPF.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CPF:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "RG:";
+            // 
+            // txtRG
+            // 
+            this.txtRG.Location = new System.Drawing.Point(159, 82);
+            this.txtRG.Name = "txtRG";
+            this.txtRG.Size = new System.Drawing.Size(141, 20);
+            this.txtRG.TabIndex = 8;
+            // 
+            // dtpDtNascimento
+            // 
+            this.dtpDtNascimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDtNascimento.Location = new System.Drawing.Point(307, 25);
+            this.dtpDtNascimento.Name = "dtpDtNascimento";
+            this.dtpDtNascimento.Size = new System.Drawing.Size(100, 20);
+            this.dtpDtNascimento.TabIndex = 9;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancelar.Location = new System.Drawing.Point(329, 130);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSalvar.Location = new System.Drawing.Point(248, 130);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 11;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
             // frmCadCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(548, 276);
+            this.ClientSize = new System.Drawing.Size(416, 165);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.dtpDtNascimento);
+            this.Controls.Add(this.txtRG);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "frmCadCliente";
-            this.Text = "frmCadCliente";
+            this.Text = "Cadastro de Cliente";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtRG;
+        private System.Windows.Forms.DateTimePicker dtpDtNascimento;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnSalvar;
     }
 }
--- AppCadastro/frmCadCliente.cs
+++ AppCadastro/frmCadCliente.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace AppCadastro
@@ -15,6 +9,195 @@ namespace AppCadastro
         public frmCadCliente()
         {
             InitializeComponent();
+        }
+
+        //Criar métodos para padronizar a exibição de mensagens
+        void MsgAtencao(string Msg)
+        {
+            //Montar um MessageBox com Icone Amarelo de Warning
+            MessageBox.Show(Msg, "Atenção", 
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            //Ex: de Uso
+            //MsgAtencao("Texto a ser exibido");
+        }
+
+        void MsgInfo(string Msg)
+        {
+            //Montar um MessageBox com Icone Azul de Information
+            MessageBox.Show(Msg, "Informação",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //Ex: de Uso
+            //MsgAtencao("Texto a ser exibido");
+        }
+
+        //Função de Manipulação de Arquivo .txt
+        //Função para validar a existencia do arquivo
+        bool ArquivoExiste(string caminho)
+        {
+            //Validar se o arquivo ja existe
+            //No caso usaremos o CPF como nome do arquivo
+            //pode ser um identificado unico para cada cadastro
+            //Desta forma se o arquivo ja existir, quer dizer
+            //que ja possui um cadastro para este CPF
+            //Utilizar o componente File e o Método Exists
+            //Se File apresentar erro
+            //basta importar a biblioteca no começo do arquivo
+            //using System.IO;
+            return File.Exists(caminho);
+        }
+
+        //Método de Gravar o Arquivo
+        void GravarArquivo(string Caminho, string Conteudo)
+        {
+            //Iremos para a rotina de gravação do arquivo
+            //Caso for o primeiro arquivo a ser cadasrado
+            //o diretorio não existira
+            //Esta diretorio será um pasta para o tipo de cadastro
+            //que será criada no mesmo local onde o programa
+            //está sendo executado
+
+            //OBS: Nao utiizaremos o Try Catch neste momento
+            //Pois estara no método principal
+
+            //Validar a existencia do diferetorio
+            //se não existir eu crio
+            //Recuperar o diretorio do parametro usando 
+            //GetDirectoryName
+            string pasta = Path.GetDirectoryName(Caminho);
+            if (!Directory.Exists(pasta))
+                Directory.CreateDirectory(pasta);
+
+            //Gravar o conteudo
+            File.WriteAllText(Caminho, Conteudo);
+        }
+
+        //Função que retorna o caminho do arquivo completo
+        string GetDirArquivo(string TipoCadastro, string NomeArquivo)
+        {
+            //O caminho será montado:
+            //Diretorio Raiz do executavel
+            //Tipo do Cadastro: Cliente
+            //Nome Arquivo: CPF
+            //Ex: C:/Programas/AppCadastros/Cliente/00000000000.txt
+
+            //Diretorio Raiz
+            //AppDomain = O Executavel em si
+            //CurrentDomain = O processo em execução (gerenciador de tarefas)
+            //BaseDirectory = local da onde o .exe está sendo executado
+            string raiz = AppDomain.CurrentDomain.BaseDirectory;
+
+            //Montar o diretorio
+            return Path.Combine(
+                raiz, TipoCadastro, NomeArquivo + ".txt");
+
+            //OBS: A extensão é opicional como tambem o uso 
+            //de arquivo de texto, pois o sistema ira ler o arquivo 
+            //bruto, a extensão não tem impacto
+            //mudar ou não usar extensão dificulta o uso de 
+            //engenharia reversa para roubo de dados
+        }
+
+        //Funação de montagem do conteudo do arquivo
+        string GetCadastro()
+        {
+            //Iremos concatenar a informação da tela
+            //em uma unica string
+            //ja no formato de cadastro
+            //assim salvando tudo no arquivo
+            //Cada linha do arquivo será um campo
+
+            string cadastro =
+                "CPF=" + txtCPF.Text +
+                Environment.NewLine + //Quebra de Linha
+                "NOME=" + txtNome.Text +
+                Environment.NewLine +
+                "DTNASC=" + dtpDtNascimento.Value +
+                Environment.NewLine +
+                "RG=" + txtRG.Text;
+
+            return cadastro;
+        }
+
+        //Método Principal
+        //Salvar o cadastro, reuni todos os demais método e funções
+        void Salvar()
+        {
+            //Primeiro precisamos montar/recuperar
+            //o diretorio do arquivo
+
+            string diretorio = GetDirArquivo("Cliente", txtCPF.Text);
+
+            //Validar a existencia do arquivo
+            //se ja existir quer dizer que possui um cadasrto
+            try
+            {
+                //Validacao
+                if(ArquivoExiste(diretorio))
+                {
+                    MsgAtencao("Já existe um cadastro com este CPF.");
+                    return; //abortar
+                }
+            }
+            catch (Exception ex)
+            {
+                MsgAtencao("Falha ao validar o arquivo." + Environment.NewLine +
+                     "Erro oringinal: " + ex.Message);
+                return; //Abortar a rotina
+            }
+
+            //Parte de gravação do arquivo
+            try
+            {
+                //Gravar o cadastro
+                GravarArquivo(diretorio, GetCadastro());
+
+                //Notificar o usuario
+                MsgInfo("Cadastro realizado com sucesso!");
+
+                //Apenas para caracter de TESTE
+                //iremos exibir uma mensagem informando onde o arquivo 
+                //foi salvo
+                //Esta mensagem não deve ir na versão final do sistema
+                MsgInfo("Salvo em:" + diretorio);
+            }
+            catch(Exception ex)
+            {
+                MsgAtencao("Falha ao salvar o cadastro." + Environment.NewLine +
+                    "Erro original: " + ex.Message);
+                //Não precisa do return para abortar por ser o final da rotina
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Salvar();
+
+            //Limpar os campos
+            txtCPF.Clear();
+            txtNome.Clear();
+            txtRG.Clear();
+
+            //Unico diferente é o campo de data
+            //pois iremos resetar a data, usando a data atual
+            //DateTime componente de Data e Hora
+            //Now retorna a data e hora atual do computador
+            dtpDtNascimento.Value = DateTime.Now;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            //Validação se realmente deseja cancelar
+            bool resultado =
+                MessageBox.Show("Deseja realmente cancelar?", 
+                                "Confirmação",
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Question)
+                                //Validando se foi clicado no botão sim
+                                == DialogResult.Yes;
+            if (resultado)
+                this.Close(); //fechamos a tela
+
+            //Se clicar em não, não realizamos nenhuma ação
         }
     }
 }
